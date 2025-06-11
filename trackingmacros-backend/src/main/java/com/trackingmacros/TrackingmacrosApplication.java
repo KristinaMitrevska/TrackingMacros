a102de80e8f88bd19a07cdd0f417669858a3901d
Kristina <kristinamitrevska267@gmail.com>
2025-06-11 20:59:31 +0200

    Setting up an empty Spring Boot project

---
+++ trackingmacros-backend/src/main/java/com/trackingmacros/TrackingmacrosApplication.java
@@ -0,0 +1,13 @@
+package com.trackingmacros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingmacrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingmacrosApplication.class, args);
+	}
+
+}
---
+++ trackingmacros-backend/src/test/java/com/trackingmacros/TrackingmacrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackingmacros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingmacrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
