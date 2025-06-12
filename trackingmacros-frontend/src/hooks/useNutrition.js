671c3d0e5ea41f492ae671e674bbd5fe8c474fa7
Kristina <kristinamitrevska267@gmail.com>
2025-06-13 00:02:03 +0200

    Implemented successful communication with backend

--- trackingmacros-frontend/src/hooks/useNutrition.js
+++ trackingmacros-frontend/src/hooks/useNutrition.js
@@ -1,7 +1,33 @@
+import {useCallback, useState} from "react";
+import nutritionRepository from "../repository/nutritionRepository.js";
+
+
+const initialState = {
+    "recipe": null,
+    "loading": false
+};
 
 
 const useNutrition = () => {
+    const [state, setState] = useState(initialState);
 
+    const onSubmit = useCallback((data) => {
+        setState(prevState => ({
+            ...prevState,
+            "loading": true
+        }));
+        nutritionRepository
+            .fetchNutrition(data)
+            .then((response) => {
+                setState({
+                    "recipe": response.data,
+                    "loading": false
+                })
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return { ...state, onSubmit };
 }
 
 export default useNutrition;
--- trackingmacros-frontend/src/ui/pages/HomePage.jsx
+++ trackingmacros-frontend/src/ui/pages/HomePage.jsx
@@ -1,10 +1,44 @@
+import {useState} from "react";
+import useNutrition from "../../hooks/useNutrition.js";
+import {Button, CircularProgress, Typography} from "@mui/material";
 
+
+const exampleData = {
+    "recipe": {
+        "ingredients": [
+            {
+            "name": "pepperoni",
+            "quantity": 200,
+            "unit": "g"
+        }
+        ]
+    }
+}
 
 const HomePage = () => {
+    const [dataState, setDataState] = useState(exampleData);
+    const {recipe, loading, onSubmit} = useNutrition();
+
+    const handleSubmit = () => {
+        onSubmit(dataState.recipe);
+    }
 
     return (
         <>
-
+            <Button onClick={handleSubmit} variant={'outlined'}>Analyze Recipe</Button>
+            {loading &&
+                <CircularProgress></CircularProgress>
+            }
+            {recipe &&
+                <>
+                    <Typography variant={'h6'}>
+                        {recipe.foods[0].name}
+                    </Typography>
+                    <Typography variant={'h6'}>
+                        {recipe.total_calories}
+                    </Typography>
+                </>
+            }
         </>
     )
 }
